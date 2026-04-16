3db5ad055cf76458b236c45a1b3d6e4ba8adae88
madhav1811 <madhav.kartheek18@gmail.com>
2026-04-16 10:33:44 +0530

    changes in api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,131 +2,67 @@ import { Holding, MockApiResponse } from '../types';
 
 export const holdingsData: Holding[] = [
   {
-    "coin": "USDC",
-    "coinName": "USDC",
-    "logo": "https://coin-images.coingecko.com/coins/images/6319/large/usdc.png?1696506694",
-    "currentPrice": 85.41,
-    "totalHolding": 0.0015339999999994802,
-    "averageBuyPrice": 1.5863185433764244,
+    "coin": "BTC",
+    "coinName": "Bitcoin",
+    "logo": "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png",
+    "currentPrice": 85320.15,
+    "totalHolding": 0.63776,
+    "averageBuyPrice": 80000,
     "stcg": {
-      "balance": 0.0015339999999994802,
-      "gain": 0.12858552735441697
+      "balance": 0.338,
+      "gain": -1200
     },
     "ltcg": {
-      "balance": 0,
-      "gain": 0
-    }
-  },
-  {
-    "coin": "WETH",
-    "coinName": "Polygon PoS Bridged WETH (Polygon POS)",
-    "logo": "https://coin-images.coingecko.com/coins/images/2518/large/weth.png?1696503332",
-    "currentPrice": 211756,
-    "totalHolding": 0.00023999998390319965,
-    "averageBuyPrice": 3599.856066001555,
-    "stcg": {
-      "balance": 0.00023999998390319965,
-      "gain": 49.957471193511736
-    },
-    "ltcg": {
-      "balance": 0,
-      "gain": 0
-    }
-  },
-  {
-    "coin": "SOL",
-    "coinName": "SOL (Wormhole)",
-    "logo": "https://coin-images.coingecko.com/coins/images/22876/large/SOL_wh_small.png?1696522175",
-    "currentPrice": 14758.01,
-    "totalHolding": 3.469446951953614e-17,
-    "averageBuyPrice": 221.42847548590152,
-    "stcg": {
-      "balance": 3.469446951953614e-17,
-      "gain": 5.043389846205066e-13
-    },
-    "ltcg": {
-      "balance": 0,
-      "gain": 0
-    }
-  },
-  {
-    "coin": "WPOL",
-    "coinName": "Wrapped POL",
-    "logo": "https://koinx-statics.s3.ap-south-1.amazonaws.com/currencies/DefaultCoin.svg",
-    "currentPrice": 22.08,
-    "totalHolding": 2.3172764293128694,
-    "averageBuyPrice": 0.5227311370876341,
-    "stcg": {
-      "balance": 1.3172764293128694,
-      "gain": 49.954151016387065
-    },
-    "ltcg": {
-      "balance": 1,
-      "gain": 20
-    }
-  },
-  {
-    "coin": "MATIC",
-    "coinName": "Polygon",
-    "logo": "https://coin-images.coingecko.com/coins/images/4713/large/polygon.png?1698233745",
-    "currentPrice": 22.22,
-    "totalHolding": 2.75145540184285,
-    "averageBuyPrice": 0.6880274617804887,
-    "stcg": {
-      "balance": 2.75145540184285,
-      "gain": 59.244262152615974
-    },
-    "ltcg": {
-      "balance": 0,
-      "gain": 0
-    }
-  },
-  {
-    "coin": "GONE",
-    "coinName": "Gone",
-    "logo": "https://koinx-statics.s3.ap-south-1.amazonaws.com/currencies/DefaultCoin.svg",
-    "currentPrice": 0.0001462,
-    "totalHolding": 696324.3075326696,
-    "averageBuyPrice": 0.00001637624055112482,
-    "stcg": {
-      "balance": 696324.3075326696,
-      "gain": 90.39943939952589
-    },
-    "ltcg": {
-      "balance": 0,
-      "gain": 0
-    }
-  },
-  {
-    "coin": "USDT",
-    "coinName": "Arbitrum Bridged USDT (Arbitrum)",
-    "logo": "https://coin-images.coingecko.com/coins/images/325/large/Tether.png?1696501661",
-    "currentPrice": 85.42,
-    "totalHolding": 0.0001580000000558357,
-    "averageBuyPrice": 1.4988059369185402,
-    "stcg": {
-      "balance": 0.0001580000000558357,
-      "gain": 0.01325954866665267
-    },
-    "ltcg": {
-      "balance": 0,
-      "gain": 0
+      "balance": 0.300,
+      "gain": 2400
     }
   },
   {
     "coin": "ETH",
     "coinName": "Ethereum",
-    "logo": "https://coin-images.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
-    "currentPrice": 216182,
-    "totalHolding": 0.0004211938732637162,
-    "averageBuyPrice": 3909.792264648455,
+    "logo": "https://coin-images.coingecko.com/coins/images/279/large/ethereum.png",
+    "currentPrice": 1620.15,
+    "totalHolding": 5.6736,
+    "averageBuyPrice": 1500,
     "stcg": {
-      "balance": 0.0004211938732637162,
-      "gain": 89.40775336229291
+      "balance": 2.332,
+      "gain": 55320.15
     },
     "ltcg": {
-      "balance": 0,
-      "gain": 0
+      "balance": 3.245,
+      "gain": 8239.29
+    }
+  },
+  {
+    "coin": "USDT",
+    "coinName": "Tether",
+    "logo": "https://coin-images.coingecko.com/coins/images/325/large/Tether.png",
+    "currentPrice": 1.15,
+    "totalHolding": 3096.54,
+    "averageBuyPrice": 1.0,
+    "stcg": {
+      "balance": 2011.23,
+      "gain": -1200
+    },
+    "ltcg": {
+      "balance": 902.47,
+      "gain": 2400
+    }
+  },
+  {
+    "coin": "MATIC",
+    "coinName": "Polygon",
+    "logo": "https://coin-images.coingecko.com/coins/images/4713/large/polygon.png",
+    "currentPrice": 2.31,
+    "totalHolding": 2210,
+    "averageBuyPrice": 0.8,
+    "stcg": {
+      "balance": 802,
+      "gain": -1200
+    },
+    "ltcg": {
+      "balance": 1402,
+      "gain": 2400
     }
   }
 ];
@@ -134,24 +70,24 @@ export const holdingsData: Holding[] = [
 export const capitalGainsData: MockApiResponse = {
   "capitalGains": {
     "stcg": {
-      "profits": 70200.88,
-      "losses": 1548.53
+      "profits": 1540,
+      "losses": 743
     },
     "ltcg": {
-      "profits": 5020,
-      "losses": 3050
+      "profits": 1200,
+      "losses": 650
     }
   }
 };
 
 export const fetchHoldings = (): Promise<Holding[]> => {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(holdingsData), 800);
+    setTimeout(() => resolve(holdingsData), 600);
   });
 };
 
 export const fetchInitialGains = (): Promise<MockApiResponse> => {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(capitalGainsData), 500);
+    setTimeout(() => resolve(capitalGainsData), 400);
   });
 };
